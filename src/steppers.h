98d1790a53e7fd83fd2d86979d8246235e4e9002
BiggusMaximus <jahekeraton164@gmail.com>
2022-12-14 19:43:29 +0700

    adding

--- src/steppers.h
+++ src/steppers.h
@@ -1,6 +1,6 @@
 #include <Stepper.h>
-
-const int stepsPerRevolution = 360;
+#define STEP_SIZE 90
+#define stepsPerRevolution 360
 
 // in1, in2, in3, in4
 Stepper VerticalStepper = Stepper(stepsPerRevolution, 6, 7, 8, 9);
@@ -12,17 +12,31 @@ void stepper_innit()
     HorizontalStepper.setSpeed(10);
 }
 
-void VstepperNol()
-{
-  VerticalStepper.step(0);
-}
+void Tracking(){
+  // For Vertical
+  if (diff_vertical > THRESHOLD){
+    // If LDR Right > LDR Left
+    VerticalStepper.step(STEP_SIZE);
+  }
+  else if (diff_vertical < -THRESHOLD){
+    // If LDR Right < LDR Left
+    VerticalStepper.step(-STEP_SIZE);
+  }else{
+    VerticalStepper.step(0);
+    VerticalStepper.setSpeed(0);
+  }
 
-void VstepperUp()
-{
-  VerticalStepper.step(90);
-}
+  // For Horizontalss
 
-void VstepperDown()
-{
-   VerticalStepper.step(-90);
+  if (diff_horizontal > THRESHOLD){
+    // If LDR Right > LDR Left
+    HorizontalStepper.step(STEP_SIZE);
+  }
+  else if (diff_horizontal < -THRESHOLD){
+    // If LDR Right < LDR Left
+    HorizontalStepper.step(-STEP_SIZE);
+  }else{
+    HorizontalStepper.step(0);
+    HorizontalStepper.setSpeed(0);
+  }
 }
