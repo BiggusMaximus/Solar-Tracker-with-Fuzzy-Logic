37ad71f3adbdd14bc55c37c6435df52f2b8c7acf
faizdaffa <faizdafa15@gmail.com>
2022-12-14 18:49:35 +0700

    Coba Stepper

--- src/main.cpp
+++ src/main.cpp
@@ -19,23 +19,21 @@ void setup()
   Serial.begin(9600);
   ldr_innit();
   stepper_innit();
+
+  Serial.println("start to zero degree");
+  delay(3000);
+  VstepperNol();
+  Serial.println("Finish to zero, start rotate 90");
+  delay(2000);
+  VerticalStepper.step(90);
+  Serial.println("start rotate -90");
+  delay(1000);
+  VerticalStepper.step(-120);
+  Serial.println("Finish to rotate -90");
 }
 
 void loop()
 {
   read_value_ldr();
-  show_info("show_ldr");
-
-  if(top == 1)
-  {
-    VstepperUp();
-  }
-  if(top == 0)
-  {
-    VstepperDown();
-  }
-  else
-  {
-    VstepperNol();
-  }
+  // show_info("show_ldr");
 }
--- src/steppers.h
+++ src/steppers.h
@@ -1,6 +1,6 @@
 #include <Stepper.h>
 
-const int stepsPerRevolution = 200;
+const int stepsPerRevolution = 360;
 
 // in1, in2, in3, in4
 Stepper VerticalStepper = Stepper(stepsPerRevolution, 6, 7, 8, 9);
@@ -8,24 +8,21 @@ Stepper HorizontalStepper = Stepper(stepsPerRevolution, 10, 11, 12, 13);
 
 void stepper_innit()
 {
-    VerticalStepper.setSpeed(60);
-    HorizontalStepper.setSpeed(60);
+    VerticalStepper.setSpeed(10);
+    HorizontalStepper.setSpeed(10);
 }
 
 void VstepperNol()
 {
-  VerticalStepper.step(100);
   VerticalStepper.step(0);
 }
 
 void VstepperUp()
 {
-  VerticalStepper.step(200);
-  VerticalStepper.step(0);
+  VerticalStepper.step(90);
 }
 
 void VstepperDown()
 {
-   VerticalStepper.step(-200);
-   VerticalStepper.step(0);
+   VerticalStepper.step(-90);
 }
